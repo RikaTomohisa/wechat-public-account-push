6a0208196c269231fa796168650b6d3088b8c782
RikaTomohisa <133010201+RikaTomohisa@users.noreply.github.com>
2023-05-09 20:56:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oeNrH6ArZcxOg5DOzBoO9W0gbrDE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'xC1BPgxaTpZySOtbwoM9TkJue8zu5w1MyCz4xcHrWrU',
+      useTemplateId: 'kMpI3br9pRE3SkTJa4trnEgvxo7bk4jp8i59x6nS0hg	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-04',
       festivals: [
